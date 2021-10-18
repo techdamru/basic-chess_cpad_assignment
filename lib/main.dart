f68714070629b29c66c4c1cf9609d4c81cf41bf5
Ashlaan Syed <ashlaan@amazon.com>
2021-10-18 22:09:10 +0530

    Adding Login Widget

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:basic_chess_v2/pages/home_page.dart';
 import 'package:basic_chess_v2/pages/pieces_page.dart';
 import 'package:basic_chess_v2/pages/splash_screen.dart';
 import 'package:basic_chess_v2/pages/play_game_page.dart';
+import 'package:basic_chess_v2/pages/login_screen_page.dart';
 
 void main() => runApp(new MyApp());
 
@@ -18,6 +19,7 @@ class MyApp extends StatelessWidget {
         '/home_page': (context) => HomePage(),
         '/pieces_page': (context) => PiecesPage(),
         '/play_game_page': (context) => PlayGamePage(),
+        '/login_screen_page': (context) => LoginScreen(),
       },
     );
   }
